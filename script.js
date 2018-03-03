fdab89c3db89bf30f63330fcd2bda6cc292b96a3
Harry Tomlinson <harry.t.123@hotmail.com>
2018-03-03 10:32:05 +0000

    finished

--- script.js
+++ script.js
@@ -12,12 +12,12 @@ var score = 0;
 var scoreText;
 
 function init() {
-    direction = "right";
+    direction = "down";
     score = 0;
     createSnake();
     createFood();
 
-    if(typeof gameLoop != "undefined") clearInterval(paint);
+    if(typeof gameLoop != "undefined") clearInterval(gameLoop);
     gameLoop = setInterval(paint, 50);
 }
 
@@ -37,47 +37,71 @@ function createSnake() {
         snakeArray.push({x: i, y: 0});
     }
 }
-    
-function paint() {    
-    ctx.fillStyle = "black";
+//Lets paint the snake now
+function paint()
+{
+    //To prevent the snake trail we need to paint the BG on every frame
+    //Lets paint the canvas BG
+    ctx.fillStyle = "white";
     ctx.fillRect(0, 0, w, h);
-    ctx.strokeStyle = "white";
+    ctx.strokeStyle = "black";
     ctx.strokeRect(0, 0, w, h);
     
+    //The movement code comes here
+    //The logic is to take out the tail cell of the snake
+    //and position it on the new head position
     var nx = snakeArray[0].x;
     var ny = snakeArray[0].y;
-
-    if(direction == "right") nx ++;
-    else if (direction == "left") nx --;
-    else if (direction == "up") ny --;
-    else if (direction == "down") ny ++;
-
-    if (nx == -1 || nx == w/10 || ny == -1 || ny == h/10 || snakeCollision(nx, ny, snakeArray)) {
+    
+    if(direction == "right") nx++;
+    else if(direction == "left") nx--;
+    else if(direction == "up") ny--;
+    else if(direction == "down") ny++;
+    
+    //Lets add the game over clauses now
+    //Game over clauses come after the new head position calculation
+    //Time to add body collision check
+    if(nx == -1 || nx == w/10 || ny == -1 || ny == h/10 || snakeCollision(nx, ny, snakeArray))
+    {
+        //restart the game
         init();
+        //Lets organize the code a bit now.
         return;
     }
-
-    if (nx == food.x && ny == food.y) {
+    
+    //Code to make the snake eat the food
+    //The logic is that if the new head position matches with the food position
+    //then instead of transferring the tail to the new head, create a new head
+    if(nx == food.x && ny == food.y)
+    {
         var tail = {x: nx, y: ny};
+        //recreate food
         createFood();
-        score ++
-    } else {
-        var tail = snakeArray.pop;
+        score++;
+    }
+    else
+    {
+        //Lets do the cell transfer now
+        var tail = snakeArray.pop(); 
+        //pop throws out the last element of an array
         tail.x = nx; tail.y = ny;
     }
-
     snakeArray.unshift(tail);
-
-    for (var i = 0; i < snakeArray.length; i ++) {
+    //Now if the snake eats the food, it gets longer.
+    
+    for(var i = 0; i < snakeArray.length; i++)
+    {
         var c = snakeArray[i];
         drawFood(c.x, c.y);
+        //Thats the snake.
     }
-
+    
+    //Lets paint the food
     drawFood(food.x, food.y);
-
-    scoreText = "Score " + score;
+    //We can see the food now.
+    
+    scoreText = "Score: " + score;
     ctx.fillText(scoreText, 5, h-5);
-
 }
 
 function drawFood(x, y) {
